1ac1b8b3506bba8d42b2b8b336626a9f5a24f6b8
Adrian Flores Herrera <adrian_fh_220699@outlook.com>
2026-09-10 16:31:53 -0600

    Displaying Remaining And Overspending Amounts

---
+++ BudgetApp/Views/BudgetSumaryView.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetSumaryView.swift
+//  BudgetApp
+//
+//  Created by Adrian Flores Herrera on 9/10/26.
+//
+
+import SwiftUI
+
+struct BudgetSumaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BudgetSumaryView()
+}
